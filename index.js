a6f2a2e19124b6fd13bc87205c11f4512cf23a3f
tengxong <119726458+tengxong@users.noreply.github.com>
2023-01-22 16:46:03 +0700

    updata 2

--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const express = require('express')
 const app = express()
 const bodyParse = require('body-parser')
-const mysql = require ('mysql')
-const cookieParser = require ('cookie-parser')
+const mysql = require('mysql')
+const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const port = 3000
 
 const connection = mysql.createConnection({
-  host: 'mydb.cu6unrorcuye.us-east-1.rds.amazonaws.com',
-  user: 'admin',
-  password: '1234jack',
-  database: 'vite',
+    host: 'mydb.cu6unrorcuye.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: '1234jack',
+    database: 'vite',
 })
 
 connection.connect()
 
-app.use(bodyPaser.urlencoded({extended:false}));
-  app.use(bodyPaser.json());
+app.use(bodyPaser.urlencoded({ extended: false }));
+app.use(bodyPaser.json());
 
-  app.use(cookieParser())
+app.use(cookieParser())
 
-app.get('/', (req, res)=> {
-    res.send('hello')
+app.get('/:id', (req, res) => {
+    var id = req.params.id;
+    var params = [id]
+
+    var sql = 'SELECT * FROM user WHERE id'
+
+    connection.query(sql, params, (err, rows, fields) => {
+        if (err) throw err
+        res.send(rows)
+    })
 })
 
 app.listen(port)
