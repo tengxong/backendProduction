28687ac8ce9f9897a573eedf5124be3ba160fe86
tengxong <119726458+tengxong@users.noreply.github.com>
2023-01-22 16:41:50 +0700

    updata 1

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const app = express()
+const bodyParse = require('body-parser')
+const mysql = require ('mysql')
+const cookieParser = require ('cookie-parser')
+const cors = require('cors')
 const port = 3000
+
+const connection = mysql.createConnection({
+  host: 'mydb.cu6unrorcuye.us-east-1.rds.amazonaws.com',
+  user: 'admin',
+  password: '1234jack',
+  database: 'vite',
+})
+
+connection.connect()
+
+app.use(bodyPaser.urlencoded({extended:false}));
+  app.use(bodyPaser.json());
+
+  app.use(cookieParser())
 
 app.get('/', (req, res)=> {
     res.send('hello')
